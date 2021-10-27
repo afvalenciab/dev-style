483ae21f3a4c2729c3c4bac2b07aff3ed0ecfc99
Andres Felipe Valencia Benitez <afvalenciab@gmail.com>
2021-10-27 08:26:43 -0500

    Update termns and policies

--- utils/product.js
+++ utils/product.js
@@ -1,5 +1,7 @@
 export const sizeList = ['S', 'M', 'L', 'XL'];
+
 export const GENDER_TYPE = { ALL: 'ALL', MEN: 'M', WOMEN: 'W' };
+
 export const priceFormatter = price => {
   const formatter = new Intl.NumberFormat('en', {
     style: 'currency',
